577407d3e44d64282af8d6805b40e5a6d52e402b
BlaktankmoanPS <153841371+BlaktankmoanPS@users.noreply.github.com>
2024-01-04 13:55:41 -0500

    Update Add PC Serial to ADObject.ps1
    As the old version was written, it would pull the serial number of whatever computer you were running the script from.  The new version will pull the serial of the computer you specify correctly.

--- Add PC Serial to ADObject.ps1
+++ Add PC Serial to ADObject.ps1
@@ -1,30 +1,29 @@
-# Prompt for computer name
-$computerName = Read-Host "Enter the computer name"
+# Prompt user for computer name
+$computerName = Read-Host -Prompt "Enter the computer name"
 
-# Prompt for computer type
-$computerType = Read-Host "Enter the computer type (Laptops, Desktops, Surface Tablets)"
+# Use WMI to retrieve the serial number
+try {
+    $serialNumber = Get-WmiObject -Class Win32_BIOS -ComputerName $computerName | Select-Object -ExpandProperty SerialNumber
+    if ($serialNumber -ne $null) {
+        Write-Host "Serial number for computer '$computerName' is: $serialNumber"
 
-# Determine the appropriate OU based on the computer type
-switch ($computerType) {
-    "Laptops" { $ou = "Laptops" }
-    "Desktops" { $ou = "Desktops" }
-    "Surface Tablets" { $ou = "Surface Tablets" }
-    default {
-        Write-Host "Invalid computer type entered."
-        exit
+        # Set the AD attribute to update
+        $adAttributeName = "serialNumber"
+
+        # Construct the LDAP path for the computer
+        $ldapPath = "LDAP://" + (Get-ADComputer -Filter {Name -eq $computerName}).DistinguishedName
+
+        # Get the AD computer object
+        $adComputer = [ADSI] $ldapPath
+
+        # Set the serial number to the specified attribute
+        $adComputer.Put($adAttributeName, $serialNumber)
+        $adComputer.SetInfo()
+
+        Write-Host "Serial number '$serialNumber' has been written to the computer '$computerName' AD object attribute '$adAttributeName'."
+    } else {
+        Write-Host "Unable to retrieve serial number for computer '$computerName'."
     }
+} catch {
+    Write-Host "Error: $_"
 }
-
-# Construct the DN (Distinguished Name) based on the computer name and type
-$computerDN = "CN=$computerName, OU=$ou, OU=Workstations, DC=PremPnut, DC=local"
-
-# Retrieve serial number using wmic
-$serialNumber = Invoke-Command { cmd.exe /c "wmic bios get serialnumber" } | Out-String
-$serialNumber = $serialNumber -ireplace "SerialNumber", ""
-$serialNumber = $serialNumber -ireplace "`n", ""
-$serialNumber = $serialNumber -ireplace "`r", ""
-$serialNumber = $serialNumber -ireplace " ", ""
-$serialNumber = $serialNumber -ireplace "`t", ""
-
-# Set the serialNumber attribute in Active Directory
-Set-ADObject -Identity $computerDN -Replace @{serialNumber = $serialNumber}
