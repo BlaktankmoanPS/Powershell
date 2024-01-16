b4eb2c1a11ef6f4a3444b5b8cc6d77562a6b6956
BlaktankmoanPS <153841371+BlaktankmoanPS@users.noreply.github.com>
2024-01-16 05:53:52 -0800

    Add files via upload

---
+++ Get Computer Name and Serial.ps1
@@ -0,0 +1,25 @@
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Specify the OU path
+$ouPath = "OU=Workstations,DC=PremPnut,DC=local"
+
+# Get all computers from the specified OU
+$computers = Get-ADComputer -Filter * -SearchBase $ouPath -Properties Name, SerialNumber
+
+# Output computer name and serialNumber to CSV file
+$outputFile = "C:\Data\serials.csv"
+$outputData = @()
+
+foreach ($computer in $computers) {
+    $computerName = $computer.Name
+    $serialNumber = $computer.SerialNumber -join ','  # Convert collection to string
+
+    $outputData += [PSCustomObject]@{
+        'ComputerName' = $computerName
+        'SerialNumber' = $serialNumber
+    }
+}
+
+$outputData | Export-Csv -Path $outputFile -NoTypeInformation
+Write-Host "Results exported to $outputFile"
